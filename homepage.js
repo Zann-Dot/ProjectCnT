92fddd81868aff38d49acec000deaaff022dc7d7
Zann-Dot <149945178+Zann-Dot@users.noreply.github.com>
2023-12-24 19:44:45 +0530

    homepage.js

--- homepage.js
+++ homepage.js
@@ -235,9 +235,9 @@ function goToCheckoutPage() {
 function goToMenuPage() {
     window.location.href = 'MobileMenuPage.html';
 }
-// function goToMenuPage() {
-//     window.location.href = '../MobileMenuPage.html';
-// }
+function goToMenu() {
+    window.location.href = '../MobileMenuPage.html';
+}
 function goToSearchPage() {
     window.location.href = 'search.html';
 }
